074e457a69eb0d8c1dba2645681305d9fc949b31
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-07-10 06:50:53 +0530

    Added Social Utils

--- frontend/utils/Socials.ts
+++ frontend/utils/Socials.ts
@@ -1,6 +1,6 @@
 
 export const Socials={
-    linkedin: 'https://www.linkedin.com/',
-    github: 'https://github.com/',
-    twitter: 'https://twitter.com/'
+    linkedin: 'https://www.linkedin.com/in/rudra-sankha-sinhamahapatra-6311aa1bb/',
+    github: 'https://github.com/Rudra-Sankha-Sinhamahapatra/BoostHub',
+    twitter: 'https://x.com/RudraSankha'
 }
